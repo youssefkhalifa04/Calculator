d543ee61032e89cc2128e63dd7eacf54bd37abb2
Youssef <youssefkh.tv2@gmail.com>
2025-04-13 12:47:18 +0100

    nothing

--- include/calculatorHandler.php
+++ include/calculatorHandler.php
@@ -1,28 +1,23 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    
     $expression = $_POST['screen'];
 
-    
     var_dump($expression);
-    
-    
-    if (preg_match('/^[0-9+\-*/(). %]+$/', $expression) &&
-        !preg_match('/[\+\-\*\/]{2,}/', $expression)) {
+
+    if (preg_match('/\/\s*0+(?![.0-9])/', $expression)) {
+        $result = "MathError";
+    } elseif (preg_match('/^[0-9+\-*/(). %]+$/', $expression) &&
+              !preg_match('/[\+\-\*\/]{2,}/', $expression)) {
         try {
             $result = eval("return $expression;");
         } catch (Throwable $e) {
             $result = "Error";
         }
-   
-    }
-    elseif (preg_match('/\/\s*0+(?![.0-9])/', $expression)
-    ){
-        $result = "MathError";
-        
     } else {
         $result = "SynthaxError";
     }
+
     header("Location: ../index.php?result=" . urlencode($result));
     exit;
 }
+?>
