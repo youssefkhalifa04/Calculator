922837934146f6a40b1662a0208df527581d0e97
Youssef <youssefkh.tv2@gmail.com>
2025-04-13 13:26:28 +0100

    fixing

--- include/calculatorHandler.php
+++ include/calculatorHandler.php
@@ -2,21 +2,32 @@
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $expression = $_POST['screen'];
 
-    var_dump($expression);
+    // Optional for debugging
+    // var_dump($expression);
 
+    // Check for division by zero first
     if (preg_match('/\/\s*0+(?![.0-9])/', $expression)) {
         $result = "MathError";
-    } elseif (preg_match('/^[0-9+\-*/(). %]+$/', $expression) &&
-              !preg_match('/[\+\-\*\/]{2,}/', $expression)) {
+    }
+    // Validate overall expression syntax
+    elseif (
+        preg_match('/^[0-9\s()+\-*\/%.]+$/', $expression) &&    // only allowed characters
+        !preg_match('/[+\-*\/%]{2,}/', $expression) &&          // no repeated operators
+        !preg_match('/[\/*+\-]$/', trim($expression)) &&        // doesn't end with operator
+        !preg_match('/^[\/*+\-]/', trim($expression))           // doesn't start with operator
+    ) {
         try {
             $result = eval("return $expression;");
         } catch (Throwable $e) {
             $result = "Error";
         }
-    } else {
-        $result = "SynthaxError";
+    }
+    // Catch any other syntax issues
+    else {
+        $result = "SyntaxError";
     }
 
+    // Redirect with result
     header("Location: ../index.php?result=" . urlencode($result));
     exit;
 }
